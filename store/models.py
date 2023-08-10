437a06afcdbab187b468b1ef219cdcb63cfa7f02
Sina-Lotfi <sinalotfi531@gmail.com>
2023-08-11 00:15:17 +0330

    Feat | add str and ordering to models

--- store/models.py
+++ store/models.py
@@ -12,7 +12,10 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, blank=True, related_name='+')  
-
+    def __str__(self) -> str:
+        return self.title
+    class Meta:
+        ordering = ['title']
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(default='-')
@@ -22,7 +25,10 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
-
+    def __str__(self) -> str:
+        return self.title
+    class Meta:
+        ordering = ['title']
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
