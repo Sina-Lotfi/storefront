0a767f2f32597c97f11d265c41b70671ee6a4cce
Sina-Lotfi <sinalotfi531@gmail.com>
2023-08-11 00:05:53 +0330

    Refactor | add new apps

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground',
+    #new
     'debug_toolbar',
+    #my_apps
+    'playground',
+    'tags',
+    'store',
+    'likes'
 ]
 
 MIDDLEWARE = [
@@ -83,8 +88,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
