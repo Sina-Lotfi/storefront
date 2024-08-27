58c85f6fcdf24a7a373d4b45b4ff03e53f4da084
Sina-Lotfi <sinalotfi531@gmail.com>
2024-08-27 21:49:10 +0330

    add new codes

---
+++ storefront/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings")
+
+celery = Celery("storefront", broker_connection_retry_on_startup=True)
+celery.config_from_object("django.conf:settings", namespace="CELERY")
+celery.conf.broker_connection_retry_on_startup = True
+celery.autodiscover_tasks()
