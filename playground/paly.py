b7d624aa9930fbe218f626d293d1f6a259618205
Sina-Lotfi <sinalotfi531@gmail.com>
2024-08-30 03:39:03 +0330

    nothing

--- playground/paly.py
+++ playground/paly.py
@@ -1,7 +1,2 @@
-def sina(request, dash, *args, **kwargs):
-    print(kwargs)
-    print(args)
-    return request
-
-
-sina(54, "sina", 2, kwargs={"sina": "mame", "prisa": 2})
+sina = 'dash {sex}'.format(sex='dash')
+print(sina)
