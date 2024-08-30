82d03bd4e12feca667040c35481b811f3722a598
Sina-Lotfi <sinalotfi531@gmail.com>
2024-08-30 03:39:26 +0330

    play ground view

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+import requests
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from .tasks import sina
+import logging
 
-def say_hell(request):
-    sina.delay('hello')
-    return render(request,'<h1>sina</h1>')
+logger = logging.getLogger(__name__)
+
+
+class TestChacheView(APIView):
+    # @method_decorator(cache_page(5 * 0))
+    def get(self, request):
+        logger.warning("salam")
+        response = requests.get("https://httpbin.org/delay/2")
+        data = response.json()
+        return Response({"detail": "dash"})
